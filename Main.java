045d2ee0424937b307a49c07ff309c87e241ca48
Carlos <perdomomoralescarlos@gmail.com>
2026-03-09 12:38:53 +0000

    Añadida la distribuvion de carpetas, añadidos MAIN.java y Nota.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+//import ui.LoginFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //no implementado
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            //new LoginFrame();
+        });
+
+    }
+}
